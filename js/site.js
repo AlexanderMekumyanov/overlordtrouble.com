d63eb8c0d371ce66549558352ad77b3999879043
Ayaal <uraankhayayaal@gmail.com>
2016-10-12 22:25:54 +0900

    Готовый дизайн, содержимое на анг

--- js/site.js
+++ js/site.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
       $('.parallax').parallax();
-    });
+
+
+       var options = [
+       		//{selector: '#staggered-test', offset: 50, callback: function(el) {Materialize.toast("This is our ScrollFire Demo!", 1500 ); } }, 
+		    //{selector: '#staggered-test', offset: 205, callback: function(el) { Materialize.toast("Please continue scrolling!", 1500 ); } },
+		    {selector: '.description', offset: 0, callback: function(el) { Materialize.showStaggeredList($(el)); } }
+		    //{selector: '#image-test', offset: 500, callback: function(el) { Materialize.fadeInImage($(el));}}
+   		]; 
+       Materialize.scrollFire(options);
+
+
+
+});
